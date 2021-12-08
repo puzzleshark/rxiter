33cef48adc1527bae239bc89b696f351178e8ca5
Samuel Banning <samcbanning@gmail.com>
2021-12-07 23:27:37 -0500

    better

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     license="MIT",
     version="0.0.1",
     packages=["rxiter"],
-    install_requires=["asyncio"],
     description="Observable operations for async generators",
     classifiers=[
         "Development Status :: 6 - Mature",
---
+++ tests/test_share.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from rxiter import share
+
+async def test_basic_share():
+
+    @share
+    async def count():
+        c = 0
+        while True:
+            yield c
+            await asyncio.sleep(1)
+            c += 1
+
+    async def count_squared():
+        async for c in count():
+            yield c ** 2
+
+    async for c in count_squared():
+        print(c)
+        if c > 10:
+            continue
