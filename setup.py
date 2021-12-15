614b9c9e8e588385038570c6514f783c49b4e3b6
Sam Banning <samcbanning@gmail.com>
2021-12-14 19:22:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="rxiter",
     license="MIT",
-    version="0.0.3",
+    version="0.0.4",
     packages=["rxiter"],
 #     install_requires=["functools"],
     description="Observable operations for async generators",
