5afcae50be05cde85d189db208e95c39c391344d
Samuel Banning <samcbanning@gmail.com>
2022-07-29 18:29:03 -0400

    progress

--- rxiter/main.py
+++ rxiter/main.py
@@ -1,10 +1,18 @@
 import functools
-# from typing import AsyncIterable, AsyncIterator
+from typing import AsyncIterable, AsyncIterator, Callable, Union
 
 from .tools import SharableAsyncIterable, ReplayAsyncIterable
 
 
-def share(obj):
+def share(obj: Union(AsyncIterable, Callable)):
+    """share can be applied to either an AsyncIterable or a Callable that returns an AsyncIterable.
+
+    Args:
+        obj (AsyncIterable | Callable): the object to share.
+
+    Returns:
+        SharableAsyncIterable: returns an SharableAsyncIterable or a Function that returns an AsyncIterable.
+    """
     if callable(obj):
         return _async_share_dec(obj)
     elif hasattr(obj, "__aiter__"):
@@ -13,7 +21,15 @@ def share(obj):
         raise Exception("share can only be applied to an AsyncIterable object.")
 
 
-def repeat(obj):
+def repeat(obj: Union(AsyncIterator, Callable)):
+    """converts a AsyncIterator back into a AsyncIterable that will repeat the same pattern.
+
+    Args:
+        obj (AsyncIterator | Callable): the object to repeat.
+
+    Returns:
+        AsyncIterable: returns an async iterable that generates this async iterator.
+    """
     if callable(obj):
         return _async_replay_dec(obj)
     elif hasattr(obj, "__anext__"):
@@ -22,15 +38,16 @@ def repeat(obj):
         raise Exception("repeat can only be applied to an AsyncIterator object.")
 
 
-def _async_share(async_iterable):
+def _async_share(async_iterable: AsyncIterable):
     return SharableAsyncIterable(async_iterable)
 
 
-def _async_replay(async_iterator):
+def _async_replay(async_iterator: AsyncIterable):
     return ReplayAsyncIterable(async_iterator)
 
 
-def _async_share_dec(async_iterable_fn):
+def _async_share_dec(async_iterable_fn: Callable):
+    @functools.wraps(async_iterable_fn)
     def wrapper(*args, **kwargs):
         obj = async_iterable_fn(*args, **kwargs)
         if not hasattr(obj, "__aiter__"):
@@ -39,7 +56,8 @@ def _async_share_dec(async_iterable_fn):
     return functools.lru_cache(maxsize = None)(wrapper)
 
 
-def _async_replay_dec(async_iterator_fn):
+def _async_replay_dec(async_iterator_fn: Callable):
+    @functools.wraps(async_iterator_fn)
     def wrapper(*args, **kwargs):
         obj = async_iterator_fn(*args, **kwargs)
         if not hasattr(obj, "__anext__"):
--- tests/test_share.py
+++ tests/test_share.py
@@ -2,30 +2,35 @@ import asyncio
 
 from rxiter import share
 
-class TestShare:
+async def test_basic_share():
+    """
+    Test that when two observables depend on one, the first one is not executed twice.
+    """
 
-    async def test_basic_share():
-        print("what")
+    total = []
 
-        @share
-        async def count():
-            v = 0
-            while True:
-                print(f"returning value {v}")
-                yield v
-                await asyncio.sleep(1)
-                v += 1
+    @share
+    async def count():
+        v = 0
+        while True:
+            yield v
+            total.append(v)
+            await asyncio.sleep(1)
+            v += 1
 
-        async def count_squared():
-            async for v in count():
-                yield v ** 2
+    async def count_squared():
+        async for v in count():
+            yield v ** 2
 
-        async def count_cubed():
-            async for v in count():
-                yield v ** 3
-        
-        for i in range(10):
-            squares = aiter(count_squared())
-            cubes = aiter(count_cubed())
-            s = await anext(squares)
-            await anext(cubes)
+    async def count_cubed():
+        async for v in count():
+            yield v ** 3
+    
+    square_iter = count_squared()
+    cube_iter = count_cubed()
+    for i in range(10):
+        s = await anext(square_iter)
+        c = await anext(cube_iter)
+    
+    assert len(total) == 10
+
