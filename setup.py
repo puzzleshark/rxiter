990531696967e0ed9b5dbab5c915fb7b43a6ed5f
Samuel Banning <samcbanning@gmail.com>
2022-07-29 18:49:51 -0400

    progress

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 with open("README.md") as fp:
     long_description = fp.read()
@@ -6,15 +6,10 @@ with open("README.md") as fp:
 setup(
     name="rxiter",
     license="MIT",
-<<<<<<< HEAD
     version="0.0.6",
     packages=["rxiter"],
     long_description=long_description,
     long_description_content_type='text/markdown',
-=======
-    version="0.0.5",
-    packages=["rxiter"],
->>>>>>> 919dcded0f847430df67ad87f663fa1f047d1b55
     description="Observable operations for async generators",
     classifiers=[
         "Development Status :: 6 - Mature",
